e9fc1ef9c55a9e9d4d9b1eba0faafd3f9bd126c9
spezdmtest <spezd.m.@gmail.com>
2024-03-20 17:47:38 +0200

    modified.js

--- weakmap.js
+++ weakmap.js
@@ -1,3 +1,15 @@
-let obj = {name: "weakmap"};
+let obj = { name: "weakmap" };
+// const arr = [obj];
+// obj = null;
+// console.log(obj); // null
+// console.log(arr); // [ { name: 'weakmap' } ]
+
+const map = new WeakMap([[obj, "obj data"]]);
+// get set delete has
+console.log(map.has(obj)); // true
+console.log(map.get(obj)); // 'obj data'
+
 obj = null;
-console.log(obj);
+console.log(map.get(obj)); // underfined
+console.log(map.has(obj)); // false
+console.log(map) // WeakMap {} 
