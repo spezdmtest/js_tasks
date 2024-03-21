536cda64bbbcb607f92309e705a25d0de618327d
spezdmtest <spezd.m.@gmail.com>
2024-03-21 12:11:00 +0200

    add weakset.js

---
+++ weakset.js
@@ -0,0 +1,7 @@
+const users = [{ name: "Elena" }, { name: "Alex" }, { name: "Irina" }];
+
+const visits = new WeakSet();
+visits.add(users[0]).add(users[1]);
+
+console.log(visits.has(users[0]));
+console.log(visits.has(users[1]));
