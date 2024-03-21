f44d3dceda946ce72f0f951be0025df2e0a4b389
spezdmtest <spezd.m.@gmail.com>
2024-03-21 11:49:44 +0200

    modified weakmap.js

--- weakmap.js
+++ weakmap.js
@@ -12,4 +12,24 @@ console.log(map.get(obj)); // 'obj data'
 obj = null;
 console.log(map.get(obj)); // underfined
 console.log(map.has(obj)); // false
-console.log(map) // WeakMap {} 
+console.log(map); // WeakMap {}
+
+// ======================================================================
+const cache = new WeakMap();
+function cacheUser(user) {
+  if (!cache.has(user)) {
+    cache.set(user, Date.now());
+  }
+   return cache.get(user);
+}
+
+let lena = {name: "Elena"}
+let alex = {name: "Alex"}
+
+cacheUser(lena);
+cacheUser(alex);
+
+lena = null;
+
+console.log(cache.has(lena)); // false
+console.log(cache.has(alex)); // true
