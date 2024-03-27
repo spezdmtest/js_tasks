c6f7fd5d1168de4afb315d7cc5b19099efa765a2
spezdmtest <spezd.m.@gmail.com>
2024-03-27 14:57:51 +0200

    add task 2

--- tasks.js
+++ tasks.js
@@ -54,3 +54,37 @@ console.log(isUnique('abcdef')) // -> true
 console.log(isUnique('1234567')) // -> true
 console.log(isUnique('abcABC')) // -> true
 console.log(isUnique('abcadef')) // -> false 
+
+/** 2 Плоский массив
+Напишите функцию, принимающая массив с вложенными массивами и 
+распакуйте в результирующий плоский массов. 
+В результате должны получить новый одномерный массив.
+Input: Array
+Output: Array
+function flatten(array) {
+  // todo
+}
+
+console.log(flatten([[1], [[2, 3]], [[[4]]]])) // -> [1, 2, 3, 4]
+ */
+
+
+function flatten(array) {
+  const res = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      //   for (let j = 0; j < array[i].length; j++) {
+      //     res.push(array[i][j]);
+      //   }
+      const flat = flatten(array[i]);
+      for (let j = 0; j < flat.length; j++) {
+        res.push(flat[j]);
+      }
+    } else {
+      res.push(array[i]);
+    }
+  }
+  return res;
+}
+
+console.log(flatten([[1], [[2, 3]], [[[4]]]])); // -> [1, 2, 3, 4]
