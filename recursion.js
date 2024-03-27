079ed372ae3a088b6b721339a76af3ac18acf113
spezdmtest <spezd.m.@gmail.com>
2024-03-27 15:08:49 +0200

    add recursion.js

---
+++ recursion.js
@@ -0,0 +1,9 @@
+// Что такое рекурсия
+// Факториал - это
+// 5! = 5*4*3*2*1 = 120;
+function factorial(num) {
+  if (num === 1) return num;
+  return num * factorial(num - 1);
+}
+
+factorial(5); // 120
