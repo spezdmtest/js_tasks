aefae39e58b035db1bc621f3f094b53f0c0bc8cc
spezdmtest <spezd.m.@gmail.com>
2024-04-02 11:37:30 +0300

    add task 4

--- tasks.js
+++ tasks.js
@@ -118,3 +118,60 @@ function removeDupes(str) {
   console.log(removeDupes("aabbccdd")); // -> 'abcd'
   console.log(removeDupes("abcddbca")); // -> 'abcd'
   console.log(removeDupes("abababcdcdcd")); // -> 'abcd'
+
+/** 4 Какая строка встречается чаще всего
+
+Напишите функцию, которая принимает массив строк и возвращает самую частовстречающуюся строку в этом массиве. Если таких строк несколько, то нужно вернуть первую, идя слева на право.
+
+**Input**: String[]
+
+**Output**: String
+
+function highestFrequency(array) {
+	 // todo
+}
+
+console.log(highestFrequency(['a', 'b', 'c', 'c', 'd', 'e'])) // -> c
+console.log(highestFrequency(['abc', 'def', 'abc', 'def', 'abc'])) // -> abc
+console.log(highestFrequency(['abc', 'def'])) // -> abc
+console.log(highestFrequency(['abc', 'abc', 'def', 'def', 'def', 'ghi', 'ghi', 'ghi', 'ghi' ])) // -> ghi
+*/
+
+function highestFrequency(array) {
+  const map = {};
+  let maxFreq = 0;
+  let maxFreqStr = array[0];
+  for (let i = 0; i < array.length; i++) {
+    const current = array[i];
+
+    if (map[current]) {
+      map[current]++;
+    } else {
+      map[current] = 1;
+    }
+
+    if (map[current] > maxFreq) {
+      maxFreq = map[current];
+      maxFreqStr = current;
+    }
+  }
+  
+  return maxFreqStr;
+}
+
+console.log(highestFrequency(["a", "b", "c", "c", "d", "e"])); // -> c
+console.log(highestFrequency(["abc", "def", "abc", "def", "abc"])); // -> abc
+console.log(highestFrequency(["abc", "def"])); // -> abc
+console.log(
+  highestFrequency([
+    "abc",
+    "abc",
+    "def",
+    "def",
+    "def",
+    "ghi",
+    "ghi",
+    "ghi",
+    "ghi",
+  ])
+); // -> ghi
