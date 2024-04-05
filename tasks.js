dd1d9e6480f197e73f139f5d2f7ae78b88ba3e97
spezdmtest <spezd.m.@gmail.com>
2024-04-05 13:58:21 +0300

    add task 5

--- tasks.js
+++ tasks.js
@@ -175,3 +175,38 @@ console.log(
     "ghi",
   ])
 ); // -> ghi
+
+/** 5 Повернута ли строка?
+ * 
+ Напишите функцию, которая принимает 2 строки. Верните `true` если строки являются 
+ перевернутым вариантом друг друга (см. пример). Иначе верните `false`.
+ 
+ **Input**: String, String
+
+ **Output**: Boolean
+
+function isStringRotated(source, test) {
+  // todo
+}
+
+console.log(isStringRotated('javascript', 'scriptjava')) // -> true
+console.log(isStringRotated('javascript', 'iptjavascr')) // -> true
+console.log(isStringRotated('javascript', 'java')) // -> false
+*/
+
+function isStringRotated(source, test) {
+  // if (source.length !== test.length) {
+  //   return false
+  // }
+  //
+  // for (let i = 0; i < source.length; i++) {
+  //   const rotate = source.slice(i, source.length) + source.slice(0, i)
+  //
+  //   if (rotate === test) {
+  //     return true
+  //   }
+  // }
+  //
+  // return false
+  return source.length === test.length && (source + source).includes(test)
+}
